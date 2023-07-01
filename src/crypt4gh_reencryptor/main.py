39343e7086f437c15d7588a5b7bce05277419023
Sveinung Gundersen <sveinugu@gmail.com>
2023-07-01 23:31:43 +0200

    Hardcoded recrypt_header response for testing purposes

--- src/crypt4gh_reencryptor/main.py
+++ src/crypt4gh_reencryptor/main.py
@@ -1,15 +1,29 @@
+from datetime import datetime
+from typing import Union
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+from pydantic import BaseModel, validator, Field
 
 app = FastAPI()
 
 VERSION = '0.1.0'
 
 
-class ReencryptParams(BaseModel):
-    encrypted_header: str
-    reencrypt_public_key: str
+class RecryptParams(BaseModel):
+    crypt4gh_header: str = Field(..., min_length=1)
+
+
+class RecryptResponse(BaseModel):
+    crypt4gh_header: str = Field(..., min_length=1)
+    crypt4gh_compute_keypair_id: str = Field(..., min_length=1)
+    crypt4gh_compute_keypair_expiration_date: Union[datetime, str]
+
+    @validator('crypt4gh_compute_keypair_expiration_date')
+    def to_iso(cls, v):
+        if isinstance(v, str):
+            v = datetime(v)
+        return v.isoformat(timespec='minutes')
 
 
 @app.get("/info")
@@ -17,9 +31,13 @@ async def info():
     return {'name': 'crypt4gh_reencryptor', 'version': VERSION}
 
 
-@app.post("/reencrypt_header")
-async def reencrypt_header(params: ReencryptParams):
-    return ['This is from my local laptop', params.encrypted_header, params.reencrypt_public_key]
+@app.post("/recrypt_header")
+async def recrypt_header(params: RecryptParams) -> RecryptResponse:
+    return RecryptResponse(
+        crypt4gh_header= "Y3J5cHQ0Z2gBAAAAAQAAAGwAAAAAAAAAvVscAO1Vbvcyh+d2sZ2ZK++MObsAwccg81hC+kLytRWTtfOf8JnuOwo+ZbqCI41lxEcNNh3VxBSWtTL5m3YoYdnV1Sw+3ZBzvzaeYU7nMR+uVscBXdLStIMIlplvO6BAe1DwUg==",
+        crypt4gh_compute_keypair_id= "cn:b38ac81f",
+        crypt4gh_compute_keypair_expiration_date= "2023-06-30T12:15",
+    )
 
 
 origins = ["*"]
