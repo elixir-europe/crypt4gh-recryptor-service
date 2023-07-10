fb1141e278cc31ee0bb009c093d97fc995fadd08
Sveinung Gundersen <sveinugu@gmail.com>
2023-07-11 09:38:01 +1000

    Commented out compute node recrypt_header endpoint

--- src/crypt4gh_recryptor_service/compute.py
+++ src/crypt4gh_recryptor_service/compute.py
@@ -13,6 +13,7 @@ class ComputeKeyInfoParams(BaseModel):
 
 
 class ComputeKeyInfoResponse(BaseModel):
+    crypt4gh_compute_public_key: str = Field(..., min_length=1)
     crypt4gh_compute_keypair_id: str = Field(..., min_length=1)
     crypt4gh_compute_keypair_expiration_date: Union[datetime, str]
 
@@ -38,19 +39,23 @@ async def info(settings: Annotated[ComputeSettings, Depends(get_compute_settings
 
 @app.post('/get_compute_key_info')
 async def get_compute_key_info(params: ComputeKeyInfoParams) -> ComputeKeyInfoResponse:
+    # user_public_key = params.crypt4gh_user_public_key
 
     return ComputeKeyInfoResponse(
+        crypt4gh_compute_public_key='-----BEGIN CRYPT4GH PUBLIC KEY-----\n'
+        '2fppz5Q7QMHVLm3eCJp2+sbjCGyDwKktjArAa+Qe9n4=\n'
+        '-----END CRYPT4GH PUBLIC KEY-----\n',
         crypt4gh_compute_keypair_id='cn:b38ac81f',
         crypt4gh_compute_keypair_expiration_date='2023-06-30T12:15',
     )
 
 
-@app.post('/recrypt_header')
-async def recrypt_header(params: ComputeRecryptParams) -> ComputeRecryptResponse:
-    return ComputeRecryptResponse(
-        crypt4gh_header='Y3J5cHQ0Z2gBAAAAAQAAAGwAAAAAAAAAwvnIV483knYvtjGVPNdxYOy0s8IMfh2kSSStkQT9Hx'
-        'ZM4J0AQzlQJdAl2LiWsvDeO7kn21J9HhUSBoieyPguM5ZcSh6s6W8anu998UTklLw5x7jMu0BNdK4yqPRue9NNiGtt'
-        'mw==',
-        crypt4gh_compute_keypair_id='cn:b38ac81f',
-        crypt4gh_compute_keypair_expiration_date='2023-06-30T12:15',
-    )
+# @app.post('/recrypt_header')
+# async def recrypt_header(params: ComputeRecryptParams) -> ComputeRecryptResponse:
+#     return ComputeRecryptResponse(
+#         crypt4gh_header='Y3J5cHQ0Z2gBAAAAAQAAAGwAAAAAAAAAwvnIV483knYvtjGVPNdxYOy0s8IMfh2kSSStkQT9Hx'
+#         'ZM4J0AQzlQJdAl2LiWsvDeO7kn21J9HhUSBoieyPguM5ZcSh6s6W8anu998UTklLw5x7jMu0BNdK4yqPRue9NNiGtt'
+#         'mw==',
+#         crypt4gh_compute_keypair_id='cn:b38ac81f',
+#         crypt4gh_compute_keypair_expiration_date='2023-06-30T12:15',
+#     )
