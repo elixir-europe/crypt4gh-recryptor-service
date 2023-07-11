1815008a4fefe1c9615030cdec8172561a679b79
Sveinung Gundersen <sveinugu@gmail.com>
2023-07-11 11:24:56 +1000

    Refactored models

--- src/crypt4gh_recryptor_service/models.py
+++ src/crypt4gh_recryptor_service/models.py
@@ -5,37 +5,32 @@ from crypt4gh_recryptor_service.validators import to_iso
 from pydantic import BaseModel, Field, validator
 
 
-class UserRecryptParams(BaseModel):
-    crypt4gh_header: str = Field(..., min_length=1)
-
-
-class UserRecryptResponse(BaseModel):
-    crypt4gh_header: str = Field(..., min_length=1)
+class ComputeKeyInfo(BaseModel):
     crypt4gh_compute_keypair_id: str = Field(..., min_length=1)
     crypt4gh_compute_keypair_expiration_date: Union[datetime, str]
 
     _to_iso = validator('crypt4gh_compute_keypair_expiration_date', allow_reuse=True)(to_iso)
+
+
+class UserRecryptParams(BaseModel):
+    crypt4gh_header: str = Field(..., min_length=1)
+
+
+class UserRecryptResponse(ComputeKeyInfo):
+    crypt4gh_header: str = Field(..., min_length=1)
 
 
 class ComputeKeyInfoParams(BaseModel):
     crypt4gh_user_public_key: str = Field(..., min_length=1)
 
 
-class ComputeKeyInfoResponse(BaseModel):
+class ComputeKeyInfoResponse(ComputeKeyInfo):
     crypt4gh_compute_public_key: str = Field(..., min_length=1)
-    crypt4gh_compute_keypair_id: str = Field(..., min_length=1)
-    crypt4gh_compute_keypair_expiration_date: Union[datetime, str]
-
-    _to_iso = validator('crypt4gh_compute_keypair_expiration_date', allow_reuse=True)(to_iso)
 
 
 # class ComputeRecryptParams(BaseModel):
 #     crypt4gh_header: str = Field(..., min_length=1)
 #
 #
-# class ComputeRecryptResponse(BaseModel):
+# class ComputeRecryptResponse(ComputeKeyInfo):
 #     crypt4gh_header: str = Field(..., min_length=1)
-#     crypt4gh_compute_keypair_id: str = Field(..., min_length=1)
-#     crypt4gh_compute_keypair_expiration_date: Union[datetime, str]
-#
-#     _to_iso = validator('crypt4gh_compute_keypair_expiration_date', allow_reuse=True)(to_iso)
